730325dcfd9366da3a42aa235400af4a761d1fa4
danielmadu <danielmadu@outlook.com>
2021-04-04 15:21:32 +0100

    initial commit

---
+++ acacium/src/main/java/com/example/acacium/AcaciumApplication.java
@@ -0,0 +1,13 @@
+package com.example.acacium;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcaciumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcaciumApplication.class, args);
+	}
+
+}
---
+++ acacium/src/test/java/com/example/acacium/AcaciumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.acacium;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcaciumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
